fd9bf90b03e0c689e085e60d36edec4457ea7879
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2022-11-07 18:18:37 +0100

    added Wizard Repository

---
+++ Assets/Scripts/Game/Repositories/WizardRepository.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class WizardRepository : MonoBehaviour
+{
+    private static int uniqueID = 0;
+
+    private Wizard[] wizards = new Wizard[2]; //simple game, 2 wizards :D
+
+    private NullWizard nullWizard = new NullWizard();
+
+    public void CreateWizard()
+    {
+        GameObject wizardGameObject = new GameObject();
+
+        Wizard wizardEntity = wizardGameObject.AddComponent<Wizard>();
+        wizardEntity.Init(uniqueID);
+
+        wizards[uniqueID] = wizardEntity;
+
+        ++uniqueID;
+    }
+
+    public IWizard GetWizard(int id)
+    {
+        if (id >= wizards.Length)
+        {
+            return nullWizard; //send null object to avoid null refs :D
+        }
+
+        return wizards[id];
+    }
+}
