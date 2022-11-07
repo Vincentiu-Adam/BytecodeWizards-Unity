88ce111053a3153b40f58f36de6f25d74ab7cac4
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2022-11-07 18:55:51 +0100

    added Main script to setup service locator and VM

---
+++ Assets/Scripts/Game/Main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private VM spellVM = new VM();
+
+    private void Awake()
+    {
+        //setup locators
+        WizardRepository wizardRepository = new WizardRepository();
+        ServiceLocator.ProvideWizardRepository(wizardRepository);
+
+        //TO DO : load some spells here then go to town :D
+    }
+}
