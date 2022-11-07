21036559daffe7f5bede4e00e996bfaadfd8b873
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2022-11-07 18:31:45 +0100

    added service locator with IWizardRepository service

---
+++ Assets/Scripts/Game/Services/ServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ServiceLocator : MonoBehaviour
+{
+    private static NullWizardRepository nullWizardRepository = new NullWizardRepository();
+
+    private static IWizardRepository wizardRepository = nullWizardRepository;
+
+    public static void ProvideWizardRepository(WizardRepository repository)
+    {
+        if (repository == null)
+        {
+            wizardRepository = nullWizardRepository;
+            return;
+        }
+
+        wizardRepository = repository;
+    }
+
+    public static IWizardRepository GetWizardRepository()
+    {
+        return wizardRepository;
+    }
+}
