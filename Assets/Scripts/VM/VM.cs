12b43056087a0da36e3d3bbff1798d8929d2b2ae
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2022-11-09 13:05:04 +0100

    BW-13: added extra subtract and divide instruction sets

--- Assets/Scripts/VM/VM.cs
+++ Assets/Scripts/VM/VM.cs
@@ -4,10 +4,14 @@ public enum Instruction : byte
 {
     //allow 64 instructions :D
     SET_LITERAL = 0xC0,
-    SET_HEALTH  = 0xC1,
-    SET_WISDOM  = 0xC2,
-    ADD         = 0xC3,
-    MULTIPLY    = 0xC4
+    GET_HEALTH  = 0xC1,
+    SET_HEALTH  = 0xC2,
+    GET_WISDOM  = 0xC3,
+    SET_WISDOM  = 0xC4,
+    ADD         = 0xC5,
+    SUBTRACT    = 0xC6, //cannot be assed to do signed bit gymnastics
+    MULTIPLY    = 0xC7,
+    DIVIDE      = 0xC8 //cannot be assed to do float gymnastics
 }
 
 public class VM
@@ -30,11 +34,27 @@ public class VM
 
                     break;
 
-                case (byte)Instruction.SET_HEALTH:
-                    int amount = Pop();
+                case (byte)Instruction.GET_HEALTH:
                     int wizardID = Pop();
 
+                    int value = GetHealth(wizardID);
+                    Push(value);
+
+                    break;
+
+                case (byte)Instruction.SET_HEALTH:
+                    int amount = Pop();
+                    wizardID = Pop();
+
                     SetHealth(wizardID, amount);
+
+                    break;
+
+                case (byte)Instruction.GET_WISDOM:
+                    wizardID = Pop();
+
+                    value = GetWisdom(wizardID);
+                    Push(value);
 
                     break;
 
@@ -54,11 +74,29 @@ public class VM
 
                     break;
 
+                case (byte)Instruction.SUBTRACT:
+                    b = Pop();
+                    a = Pop();
+
+                    Push(a - b);
+
+                    break;
+
                 case (byte)Instruction.MULTIPLY:
                     b = Pop();
                     a = Pop();
 
                     Push(a * b);
+
+                    break;
+
+                case (byte)Instruction.DIVIDE:
+                    b = Pop();
+                    a = Pop();
+
+                    //avoid division by 0
+                    b = b == 0 ? 1 : b;
+                    Push(a / b);
 
                     break;
             }
@@ -77,12 +115,28 @@ public class VM
         return stack[--stackSize];
     }
 
+    private int GetHealth(int wizardID)
+    {
+        IWizardRepository wizardRepository = ServiceLocator.GetWizardRepository();
+
+        IWizard wizard = wizardRepository.GetWizard(wizardID);
+        return wizard.Health;
+    }
+
     private void SetHealth(int wizardID, int amount)
     {
         IWizardRepository wizardRepository = ServiceLocator.GetWizardRepository();
 
         IWizard wizard = wizardRepository.GetWizard(wizardID);
         wizard.SetHealth(amount);
+    }
+
+    private int GetWisdom(int wizardID)
+    {
+        IWizardRepository wizardRepository = ServiceLocator.GetWizardRepository();
+
+        IWizard wizard = wizardRepository.GetWizard(wizardID);
+        return wizard.Wisdom;
     }
 
     private void SetWisdom(int wizardID, int amount)
